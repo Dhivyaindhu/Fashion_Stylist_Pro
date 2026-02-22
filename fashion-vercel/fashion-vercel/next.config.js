3942d6e48c053096a1d63953f70512aca58cda9b
C.Indhupriya <117663766+Dhivyaindhu@users.noreply.github.com>
2026-02-22 12:04:14 +0530

    Delete fashion-vercel/fashion-vercel directory

--- fashion-vercel/fashion-vercel/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: true },
-  images: { domains: ['api.dicebear.com'] },
-}
-module.exports = nextConfig
